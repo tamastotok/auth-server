b88a7038881722f0d2f9c1ee966598e16170af71
tamastotok <tamastotok.tk@gmail.com>
2021-12-14 06:28:33 +0100

    change req.header to req.params

--- src/controllers/user/findUserController.js
+++ src/controllers/user/findUserController.js
@@ -1,7 +1,7 @@
 const User = require('../../models/User');
 
 module.exports = async function find_user(req, res) {
-  const userData = await User.findById(req.header('_id'));
+  const userData = await User.findById(req.params.id);
   if (!userData) return res.status(400).send('User is not found!');
 
   res.send(userData);
